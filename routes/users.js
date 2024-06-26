125b53f2560f68e433d2c1cfe6bf50c9fe75e67c
zachwetsel <zachwetsel@yahoo.com>
2024-06-25 20:21:41 -0600

    added users and controllers

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const usersController = require('../controllers/users');
+
+router.get('/', usersController.getAll);
+
+router.get('/:id', usersController.getSingle);
+
+module.exports = router;
